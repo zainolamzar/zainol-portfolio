a225747f138f225f4d48e1bd3ec875628e67bcc4
Zain Coder <zainolamzar.work@gmail.com>
2025-08-28 11:34:30 +0800

    refactored projects pages

---
+++ app/blogs/page.tsx
@@ -0,0 +1,10 @@
+export default function BlogsPage() {
+    return (
+        <div className="p-4">
+        <h1 className="text-4xl font-bold mb-6">Blogs</h1>
+        <p className="text-lg max-w-2xl">
+            Welcome to the Blogs page! Here, you will find a collection of my latest articles, insights, and thoughts on various topics. I share my experiences, knowledge, and ideas to engage with readers and foster a community of learning and growth. Feel free to explore and join the conversation!
+        </p>
+        </div>
+    )
+}
